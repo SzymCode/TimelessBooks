24b9f5d1ecc3be936f0a4e2869556f1d5d268716
SzymCode <s.radomski19@gmail.com>
2024-04-29 22:47:51 +0200

    PrimeVue installed & configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 
+import 'primevue/resources/themes/aura-dark-noir/theme.css'
+
 import '@/style.css'
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+export const app = createApp(App)
+
+/**
+ *  App uses & mount
+ */
+import PrimeVue from 'primevue/config'
+
+app.use(PrimeVue).mount('#app')
