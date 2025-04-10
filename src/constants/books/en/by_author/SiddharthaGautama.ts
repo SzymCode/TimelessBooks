4f97111baa478754ee1320fd042ed7a253563bb2
SzymCode <szymon.radomski@yahoo.com>
2025-04-11 00:03:40 +0200

    Books update

---
+++ src/constants/books/en/by_author/SiddharthaGautama.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const SiddharthaGautama: BookInterface[] = [
+    {
+        author: 'Siddhartha Gautama',
+        title: 'The Dhammapada',
+        category: ['existential'],
+        url: '',
+    },
+]
+
+export default SiddharthaGautama
---
+++ src/constants/books/pl/by_author/SiddharthaGautama.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const SiddharthaGautama: BookInterface[] = [
+    {
+        author: 'Siddhartha Gautama',
+        title: 'Dhammapada',
+        category: ['egzystencjalne'],
+        url: '',
+    },
+]
+
+export default SiddharthaGautama
