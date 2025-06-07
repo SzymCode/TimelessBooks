088a05acc33e45b0ad07ea8a4096e3736e4d7d2b
SzymCode <szymon.radomski@yahoo.com>
2025-06-07 15:19:11 +0200

    Books update

---
+++ src/constants/books/en/by_author/NapoleonHill.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const NapoleonHill: BookInterface[] = [
+    {
+        author: 'Napoleon Hill',
+        title: 'Think and Grow Rich',
+        category: ['business'],
+        url: '',
+    },
+]
+
+export default NapoleonHill
---
+++ src/constants/books/pl/by_author/NapoleonHill.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const NapoleonHill: BookInterface[] = [
+    {
+        author: 'Napoleon Hill',
+        title: 'Myśl i bogać się',
+        category: ['biznes'],
+        url: '',
+    },
+]
+
+export default NapoleonHill
