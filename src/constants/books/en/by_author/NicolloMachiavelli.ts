5063f08fc58ad129d6a3d188246ca689676dfee3
SzymCode <szymon.radomski@yahoo.com>
2025-06-28 13:50:53 +0200

    Books update

---
+++ src/constants/books/en/by_author/NicolloMachiavelli.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const NicolloMachiavelli: BookInterface[] = [
+    {
+        author: 'Niccolò Machiavelli',
+        title: 'The Prince',
+        category: ['philosophy'],
+        url: '',
+    },
+]
+
+export default NicolloMachiavelli
---
+++ src/constants/books/pl/by_author/NicolloMachiavelli.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const NicolloMachiavelli: BookInterface[] = [
+    {
+        author: 'Niccolò Machiavelli',
+        title: 'Książę',
+        category: ['filozofia'],
+        url: '',
+    },
+]
+
+export default NicolloMachiavelli
