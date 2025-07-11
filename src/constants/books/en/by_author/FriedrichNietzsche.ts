27fd67bece7419452e9bc76812108e6643d82f39
SzymCode <szymon.radomski@yahoo.com>
2025-07-12 01:29:29 +0200

    Books update

---
+++ src/constants/books/en/by_author/FriedrichNietzsche.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const FriedrichNietzsche: BookInterface[] = [
+    {
+        author: 'Friedrich Nietzsche',
+        title: 'Beyond Good and Evil',
+        category: ['existential'],
+        url: '',
+    },
+]
+
+export default FriedrichNietzsche
---
+++ src/constants/books/pl/by_author/FriedrichNietzsche.ts
@@ -0,0 +1,12 @@
+import { BookInterface } from '@/types'
+
+const FriedrichNietzsche: BookInterface[] = [
+    {
+        author: 'Friedrich Nietzsche',
+        title: 'Poza dobrem i złem',
+        category: ['egzystencjalne'],
+        url: '',
+    },
+]
+
+export default FriedrichNietzsche
